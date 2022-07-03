2ee3b390375a9c31abc6ee67fbd5b96de6cb78b2
parkjunsun <qkrwnstns52@naver.com>
2022-07-03 16:54:57 +0900

    연동작업

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,11 +1,26 @@
+import {useEffect, useState} from 'react'
 import Header from "../../components/Common/Header";
 import FooterNav from "../../components/Common/FooterNav";
 import { FeedWrap } from "./style";
 import WriteButton from "../../components/Feed/WriteButton";
 import FeedContent from "../../components/Feed/Content";
 import NonMember from "../../components/Feed/Hello/NonMember";
+import axios from 'axios';
 
 const Feed = () => {
+  const [dataList, setDataList] = useState<string>('')
+
+  useEffect(() => {
+    (async () => {
+        try {
+            const { data } = await axios.get('http://localhost:8080/board/2');
+            setDataList(data.data);
+            console.log('성공입니다.')
+        } catch (error) {
+            alert(error);
+        }
+    })();
+}, []);
   return (
     <>
       <Header />
---
+++ src/setProxy.tsx
@@ -0,0 +1,10 @@
+export const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app:any) {
+  app.use(
+    createProxyMiddleware('/api/v1', {
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    })
+  );
+};
