6514ba5c303b565904f41fd38a516afcc836fdfa
jhjeong <51750678+khunjeong@users.noreply.github.com>
2022-11-02 23:09:37 +0900

    CHORE: 프록시 수정

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-};
+module.exports = (phase, { defaultConfig }) => {
+    const rewrites = () => {
+        return [
+            {
+                source: '/api/v1/:path*',
+                destination: "http://15.164.152.33:8080/api/:path*",
+            },
+        ];
+    }
+    return {
+        reactStrictMode: true,rewrites }
+}
--- src/utils/OAuth.ts
+++ src/utils/OAuth.ts
@@ -1,1 +1,2 @@
-export const KAKAO_AUTH_URL = `http://localhost:8080/oauth2/authorization/kakao`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=d7ef99010604eb04b5a9bde6d53867f4&redirect_uri=http://15.164.152.33:8080
+`;
